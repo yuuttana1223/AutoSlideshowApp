a9bdb04d93e9eb984937a0bbe45045098ffa63e8
Yuu Tanaka <yutanadreamfilel.seed@gmail.com>
2020-10-24 16:27:57 +0900

    :art: add: external appearance

--- app/src/main/java/com/example/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/com/example/autoslideshowapp/MainActivity.kt
@@ -1,11 +1,70 @@
 package com.example.autoslideshowapp
 
+import android.Manifest
+import android.content.ContentUris
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val PERMISSIONS_REQUEST_CODE = 100
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        next_button.setOnClickListener(this)
+        back_button.setOnClickListener(this)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                getContentsInfo()
+            } else {
+                requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSIONS_REQUEST_CODE)
+            }
+        } else {
+            getContentsInfo()
+        }
     }
+
+    override fun onClick(v: View) {
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSIONS_REQUEST_CODE ->
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContentsInfo()
+                }
+        }
+    }
+
+    private fun getContentsInfo() {
+        val resolver = contentResolver
+        val cursor = resolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            null,
+            null,
+            null,
+            null
+        )
+
+        if (cursor!!.moveToFirst()) {
+            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor.getLong(fieldIndex)
+            val imageUri =  ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+            imageView.setImageURI(imageUri)
+        }
+        cursor.close()
+    }
+
 }
