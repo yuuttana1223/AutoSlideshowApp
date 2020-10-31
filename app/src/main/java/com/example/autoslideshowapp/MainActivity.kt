73943fe70f3f39026775336bba74925ee2667570
Yuu Tanaka <yutanadreamfilel.seed@gmail.com>
2020-10-31 19:19:25 +0900

    :art:  add: next_button and back_button

--- app/src/main/java/com/example/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/com/example/autoslideshowapp/MainActivity.kt
@@ -3,36 +3,49 @@ package com.example.autoslideshowapp
 import android.Manifest
 import android.content.ContentUris
 import android.content.pm.PackageManager
+import android.database.Cursor
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private val PERMISSIONS_REQUEST_CODE = 100
+    private lateinit var cursor: Cursor
+    private var handler = Handler()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         next_button.setOnClickListener(this)
         back_button.setOnClickListener(this)
+        start_stop_button.setOnClickListener(this)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                getContentsInfo()
+                getImage()
             } else {
                 requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSIONS_REQUEST_CODE)
             }
         } else {
-            getContentsInfo()
+            getImage()
         }
     }
 
     override fun onClick(v: View) {
-
+        when (v.id) {
+            R.id.next_button -> getNextImage()
+            R.id.back_button -> getBackImage()
+//            R.id.start_stop_button -> start_stop_button.text = "停止"
+        }
     }
 
     override fun onRequestPermissionsResult(
@@ -43,28 +56,47 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         when (requestCode) {
             PERMISSIONS_REQUEST_CODE ->
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    getContentsInfo()
+                    getImage()
                 }
         }
     }
 
-    private fun getContentsInfo() {
-        val resolver = contentResolver
-        val cursor = resolver.query(
+    private fun getImage() {
+        cursor = contentResolver.query(
             MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
             null,
             null,
             null,
             null
-        )
+        )!!
 
-        if (cursor!!.moveToFirst()) {
-            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-            val id = cursor.getLong(fieldIndex)
-            val imageUri =  ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-            imageView.setImageURI(imageUri)
+        if (cursor.moveToFirst()) {
+            imageShow()
         }
-        cursor.close()
+    }
+
+    private fun getNextImage() {
+        if (cursor.moveToNext()) {
+            imageShow()
+        } else if (cursor.moveToFirst()) {
+            imageShow()
+        }
+    }
+
+    private fun getBackImage() {
+        if (cursor.moveToPrevious()) {
+            imageShow()
+        } else if (cursor.moveToLast()) {
+            imageShow()
+        }
+    }
+
+    private fun imageShow() {
+        val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+        val id = cursor.getLong(fieldIndex)
+        val imageUri =  ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+        Log.d("ANDROID", "URI : $imageUri")
+        imageView.setImageURI(imageUri)
     }
 
 }
